6fe075aa1a2e371e7c7eb8e33f95301a7e721c53
dheimemocelin <dheime7@gmail.com>
2023-07-30 16:54:40 -0400

    codigo adicionado

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, jsonify
+from iphone_scraper import Sit
+from samsung import Site
+from informatica import Info
+from tv import Tvs
+from eletroportateis import Eletro
+from smartwatch import Smart
+from audio import Audio
+from game import Game
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/iphones')
+def iphones():
+    site_iphone = Sit('iphone')
+    dict_iphone = site_iphone.celular()
+    return render_template('iphones.html', data=dict_iphone)
+
+@app.route('/samsung')
+def samsung():
+    site_iphone = Site('samsung')
+    dict_iphone = site_iphone.celular()
+    return render_template('samsung.html', data=dict_iphone)
+
+@app.route('/informatica')
+def informatica():
+    site_iphone = Info('informatica')
+    dict_iphone = site_iphone.informatica()
+    return render_template('informatica.html', data=dict_iphone)
+
+@app.route('/tv')
+def tv():
+    site_iphone = Tvs('tv')
+    dict_iphone = site_iphone.tv()
+    return render_template('tv.html', data=dict_iphone)
+
+@app.route('/eletroportateis')
+def eletroportateis():
+    site_iphone = Eletro('eletricos')
+    dict_iphone = site_iphone.eletroportateis()
+    return render_template('eletroportateis.html', data=dict_iphone)
+
+@app.route('/smartwatch')
+def smartwatch():
+    site_iphone = Smart('smartwatch')
+    dict_iphone = site_iphone.smartwatch()
+    return render_template('smartwatch.html', data=dict_iphone)
+
+@app.route('/audio')
+def audio():
+    site_iphone = Audio('audio')
+    dict_iphone = site_iphone.audio()
+    return render_template('audio.html', data=dict_iphone)
+
+@app.route('/games')
+def games():
+    site_iphone = Game('game')
+    dict_iphone = site_iphone.game()
+    return render_template('games.html', data=dict_iphone)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ samsung.py
@@ -0,0 +1,56 @@
+#bibliotecas
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class Site:
+    def __init__(self, opcao):
+        self.opcao = opcao
+
+    def format_price(self, price):
+        # Implemente sua lógica para formatar o preço aqui, se necessário
+        return price
+
+    def celular(self):
+        if self.opcao.lower() == 'samsung':
+            base_url = 'https://www.magazinevoce.com.br/magazinemocelinn/samsung-galaxy/celulares-e-smartphones/s/te/galx/'
+            headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+            
+            res = requests.get(base_url, headers=headers)
+            soup = BeautifulSoup(res.content, 'html.parser')
+            
+            dados_h2 = soup.find_all('div', attrs={'data-testid': 'product-card-content'})
+            dados_link = soup.find_all('li', class_='sc-gQSkpc jFWfzG')
+            dados_o = soup.find_all('p', attrs={'data-testid': ['price-original']})
+            dados_p = soup.find_all('p', attrs={'data-testid': ['price-value']})
+            div_tags = soup.find_all('div', class_='sc-fwwElh bjQZYx')
+            
+            
+            data_dict = {'titulo': [],'urls': [], 'original_prices': [], 'current_prices': [], 'image_urls': []}
+            
+            for dado in dados_h2:
+                h2 = dado.find_all('h2')
+                for titulo in h2:
+                    data_dict['titulo'].append(titulo.get_text())
+
+            for link in dados_link:
+                a_tag = link.find('a')
+                if a_tag and 'href' in a_tag.attrs:
+                    href = a_tag['href']
+                    full_url = f'https://www.magazinevoce.com.br{href}'
+                    data_dict['urls'].append(full_url)
+
+            for dado in dados_o:
+                data_dict['original_prices'].append(self.format_price(dado.text))
+
+            for dado in dados_p:
+                data_dict['current_prices'].append(self.format_price(dado.text))
+
+            for div_tag in div_tags:
+                img_tag = div_tag.find('img', attrs={'data-testid': 'image'})
+                if img_tag and 'src' in img_tag.attrs:
+                    image_url = img_tag['src']
+                    data_dict['image_urls'].append(image_url)
+
+            return data_dict
+
